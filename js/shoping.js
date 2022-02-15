0d109f7208b85ddc86ae6e8136f46b4edb1edca7
zishan344 <zishanahmed344@gmail.com>
2022-02-15 11:17:17 +0600

    done the counting number

--- js/shoping.js
+++ js/shoping.js
@@ -1,3 +1,22 @@
+function count(product, isAdd) {
+  const productInput = document.getElementById(product + "-input");
+  const productInputText = parseInt(productInput.value);
+  if (isAdd == true) {
+    productInput.value = productInputText + 1;
+  } else if (productInput.value > 1) {
+    productInput.value = productInputText - 1;
+  }
+}
+
 document.getElementById("phone-plus").addEventListener("click", function () {
-  console.log("ha ha done");
+  count("phone", true);
+});
+document.getElementById("phone-minus").addEventListener("click", function () {
+  count("phone", false);
+});
+document.getElementById("case-plus").addEventListener("click", function () {
+  count("case", true);
+});
+document.getElementById(" case-minus").addEventListener("click", function () {
+  count("case", false);
 });
